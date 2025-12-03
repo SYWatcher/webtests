70cb8da794cd9d31c52a7abb9f6a4b5cb302ca00
SYwatcher <alg0r@bk.ru>
2025-12-03 22:39:36 +0300

    Создан тест для проверки скролла на странице помощи

---
+++ tests/HelpTests.py
@@ -0,0 +1,15 @@
+from core.base_test import browser
+from pages.BasePage import BasePage
+from pages.HelpPage import HelpPageHelper, HelpPageLocators
+from pages.AdvertisementCabinet import AdvertisementCabinetHelper , AdvertisementCabinetLocators
+import allure
+
+BASE_URL = 'https://ok.ru/help'
+
+@allure.suite("Проверка скролла на странице помощи")
+@allure.title("Проверка открытия страницы 'Рекламный кабинет' после скролла")
+def test_help_test(browser):
+    BasePage(browser).get_url(BASE_URL)
+    HelpPage = HelpPageHelper(browser)
+    HelpPage.scrollToitem(HelpPageLocators.ADVERTISEMENT_CABINET)
+    AdvertisementCabinetHelper(browser)
